9649e87878ee81688aa714e64d8ad2c6c245b7f4
virsail <virsaileric@gmail.com>
2022-05-06 01:53:17 +0300

    add all changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    name: 'Virsail Mbagaya',
+    title: "Sofware Developer",
+    desciption:"There future belongs to us who believe in the beauty of our  dreams so never give up. Stay positive even in the midst of a storm because trust me you will get there eventually.",
+    image: 'https://www.google.com/search?q=what+picture+can+i+use+for+my+developer+portfolio&sxsrf=ALiCzsYB5FF8qIbT8gyDiNXtpYB51a4F4Q:1651790934961&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjDqK_fuMn3AhVpR_EDHQcbDk4Q_AUoAXoECAEQAw&biw=1528&bih=739&dpr=1#imgrc=JiH-sQlRZdm1GM',
     resumePdf: resume
 }
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,100 +1,74 @@
-import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
-
+import news2 from '../assets/svg/projects/news2.png'
+import access from '../assets/svg/projects/accessafya.png'
+import inveeen from '../assets/svg/projects/inveeen.jpeg'
+import giphy from '../assets/svg/projects/giphy.png'
+import pizza from '../assets/svg/projects/pizza.jpeg'
+import github from '../assets/svg/projects/github.png'
+import pythonshell from '../assets/svg/projects/pythonshell.png'
 
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Employee Management',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
+        projectName: 'News Application',
+        projectDesc: 'This project displays a list of various news sources. On choosing a news source, it will preview the top news articles of the day. Clicking a news article will redirect the user to read it fully from the news source. It achieves this by using the News API.',
+        tags: ['Python application'],
+        code: 'https://github.com/Virsail/News.git',
+        demo: 'https://lokeyfeellike.herokuapp.com/',
+        image: news2
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: two
+        projectName: 'Access Afya',
+        projectDesc: 'An analytics platform built on React js for UI ,Javascript ,Scss for application styling and GraphQl for api data queries.',
+        tags: ['React application'],
+        code: 'https://github.com/Virsail/AccessAfya.git',
+        demo: 'https://accessafyaplatform.netlify.app/',
+        image: access
     },
     {
         id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
+        projectName: 'Inventory Management System',
+        projectDesc: 'This is a inventory management system for tracking inventory levels, orders made, sales and product requests.',
+        tags: ['Python application',],
+        code: 'https://github.com/Virsail/Inventory-Management-System.git',
+        demo: 'https://smartshelf-solns.herokuapp.com/',
+        image: inveeen
     },
     {
         id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
+        projectName: 'Python Shell',
+        projectDesc: 'This is a python based application that manages login and signup credentials of a person for various accounts on a linux terminal i.e. username and passwords for each account.',
+        tags: ['Linux shell based project'],
+        code: 'https://github.com/Virsail/Python-Shell.git',
+        demo: 'https://github.com/Virsail/Python-Shell.git',
+        image: pythonshell
     },
     {
         id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
+        projectName: 'Github Search',
+        projectDesc: 'This is an agular cli powered project that allows the user to input the github user name and get his profile displayed ,It is powered by the github api that allowers the website to access user information from github and display public repositories of the user',
+        tags: ['Powered by Angular'],
+        code: 'https://github.com/Virsail/Github-Search.git',
+        demo: 'https://virsail.github.io/Github-Search/users',
+        image: github
     },
     {
         id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: six
+        projectName: 'Giphys For Fun',
+        projectDesc: 'This is an Angular app that allows user to search for any giphy ,,see trending giphys and see random giphys just for fun',
+        tags: ['Angular'],
+        code: 'https://github.com/Virsail/Giphy.git',
+        demo: 'https://virsail.github.io/Giphy/',
+        image: giphy
     },
     {
         id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: seven
+        projectName: 'Pizza Delivery Application',
+        projectDesc: 'Slice Of Heaven web app is an application built on JS,HTML and CSS for styling.This is a web app for a pizza shop that definitely gives the client an ultimate experience with our tasty slices.',
+        tags: ['Javascript'],
+        code: 'https://github.com/Virsail/slice-of-heaven.git',
+        demo: 'https://virsail.github.io/slice-of-heaven/',
+        image: pizza
     },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
-    },
+  
 ]
-
-
-// Do not remove any fields.
-// Leave it blank instead as shown below
-
-/* 
-{
-    id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    code: '',
-    demo: '',
-    image: ''
-}, 
-*/
