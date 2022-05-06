26d3fae0474f5266aaee0c92e25e0f1454a06c7f
virsail <virsaileric@gmail.com>
2022-05-06 04:18:00 +0300

    submit all changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,11 @@
 import resume from '../assets/pdf/resume.pdf'
-
+import mypic from '../assets/img/mypic.jpeg'
+import boss from '../assets/img/boss.jpg'
+import b00
 export const headerData = {
     name: 'Virsail Mbagaya',
     title: "Sofware Developer",
     desciption:"There future belongs to us who believe in the beauty of our  dreams so never give up. Stay positive even in the midst of a storm because trust me you will get there eventually.",
-    image: 'https://www.google.com/search?q=what+picture+can+i+use+for+my+developer+portfolio&sxsrf=ALiCzsYB5FF8qIbT8gyDiNXtpYB51a4F4Q:1651790934961&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjDqK_fuMn3AhVpR_EDHQcbDk4Q_AUoAXoECAEQAw&biw=1528&bih=739&dpr=1#imgrc=JiH-sQlRZdm1GM',
+    image: boss,
     resumePdf: resume
 }
