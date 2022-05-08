770bdb23b6859cfbd5a306243f1db365629a0e7f
virsail <virsaileric@gmail.com>
2022-05-08 11:21:50 +0300

    deploy

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import boss from '../assets/img/boss.jpg'
 export const headerData = {
     name: 'Virsail Mbagaya',
     title: "Sofware Developer",
-    desciption:"The future belongs to us who believe in the beauty of our  dreams.Everyone is a genius so be the change you wish to see in the world .",
+    desciption:"The future belongs to us who believe in the beauty of our  dreams.Everyone is a genius so be the change you wish to see in the world.",
     image: boss,
     resumePdf: resume
 }
